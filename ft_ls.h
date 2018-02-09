e3beebf8a43177b305bb27e8c7743747bfed0286
Alexandre Malfoy <almalfoy@z1r12p1.le-101.fr>
2018-02-09 18:32:17 +0100

    ls debut

---
+++ ft_ls.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_ls.h                                          .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: almalfoy <almalfoy@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/02/09 17:38:45 by almalfoy     #+#   ##    ##    #+#       */
+/*   Updated: 2018/02/09 17:43:31 by almalfoy    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef __FT_LS_H
+# define __FT_LS_H
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <dirent.h>
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
